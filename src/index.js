79a9dab158a8a1be9c2361485da06901b3e7a970
Saulo Nunes <saulo@paralink.com.br>
2020-03-18 16:55:05 -0300

    fixed the test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-"use strict";
+'use strict'
 
-import React from "react";
-import { render } from "react-dom";
-import MainRoutes from "./routes/Main.Routes";
-import "./styles/index.css";
-import "./assets/images/favicon.ico";
+import React from 'react'
+import { render } from 'react-dom'
+import MainRoutes from './routes/Main.Routes'
+import './styles/index.css'
+import './assets/images/favicon.ico'
 
-var test;
-
-render(<MainRoutes />, document.getElementById("root"));
+render(<MainRoutes />, document.getElementById('root'))
