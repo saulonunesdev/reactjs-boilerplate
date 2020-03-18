8091a77574c671f6fd0f396c9eed142d047fbc24
Saulo Nunes <saulo@paralink.com.br>
2020-03-18 16:45:18 -0300

    test to fail

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-'use strict'
+"use strict";
 
-import React from 'react'
-import { render } from 'react-dom'
-import MainRoutes from './routes/Main.Routes'
-import './styles/index.css'
-import './assets/images/favicon.ico'
+import React from "react";
+import { render } from "react-dom";
+import MainRoutes from "./routes/Main.Routes";
+import "./styles/index.css";
+import "./assets/images/favicon.ico";
 
-render(<MainRoutes />, document.getElementById('root'))
+var test;
+
+render(<MainRoutes />, document.getElementById("root"));
