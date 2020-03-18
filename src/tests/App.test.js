3de876ff326224a6ba531424cb37d697f1bf1bbd
Saulo Nunes <saulo@paralink.com.br>
2020-03-18 13:44:26 -0300

    Teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from '../pages/App'
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "../pages/App";
 
-window.test('renders learn react link', () => {
-  const { getByText } = render(<Router><App /></Router>)
-  const linkElement = getByText(/Learn React/i)
-  window.expect(linkElement).toBeInTheDocument()
-})
+window.test("renders learn react link", () => {
+  const { getByText } = render(
+    <Router>
+      <App />
+    </Router>
+  );
+  const linkElement = getByText(/Learn XReact/i);
+  window.expect(linkElement).toBeInTheDocument();
+});
